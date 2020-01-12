f823b21ca54b13b2433038ee6173b90dc27269ce
Renzo <rcnavarrop@gmail.com>
2020-01-12 12:26:17 -0500

    Adds and centers UploadContainer

--- src/components/dragAndDropUploader.tsx
+++ src/components/dragAndDropUploader.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const DragAndDropUploader = () => {
-return <>This is a DragAndDropUploader</>
-}
+  return (
+    <Screen>
+      <UploadContainer>This is a DragAndDropUploader</UploadContainer>;
+    </Screen>
+  );
+};
 
-export {DragAndDropUploader};
+const Screen = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100wh;
+`;
+
+const UploadContainer = styled.div`
+  width: 400px;
+  height: 590px;
+  background-color: red;
+`;
+
+export { DragAndDropUploader };
